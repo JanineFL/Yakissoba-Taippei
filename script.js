1681b8a8e4d5d0194d61c1ab2c6ebeb262216224
= <=>
2025-05-11 17:15:20 -0300

    foi adicionado a pagina de carrinhos afim de gerenciar pedidos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //fazer botoes onde leva a teg de comida, bebida, etc
 //fazer o "carrinho" ingual o do ifood
 //fazer o login e o usuario
-//estruturar o carrinho e ainda continuar com as informaçoes
+//estruturar o carrinho e ainda continuar com as informaçoes oi
